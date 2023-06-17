6dde473a0e010129446a42f815464da27c2f790d
bayfrs <113220899+bayfrs@users.noreply.github.com>
2023-06-17 15:50:41 +0700

    update fftdmbetaauto.js

--- tools/fftdmbetaauto.js
+++ tools/fftdmbetaauto.js
@@ -3,16 +3,7 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
 (async () => {
     print(
         chalk`{bold.green
-  ▄▄▄▄▄            ▄▄▌  .▄▄ · ▪   ▄▄ • 
-  •██  ▪     ▪     ██•  ▐█ ▀. ██ ▐█ ▀ ▪
-   ▐█.▪ ▄█▀▄  ▄█▀▄ ██▪  ▄▀▀▀█▄▐█·▄█ ▀█▄  [fftdm]
-   ▐█▌·▐█▌.▐▌▐█▌.▐▌▐█▌▐▌▐█▄▪▐█▐█▌▐█▄▪▐█  [BETA]
-   ▀▀▀  ▀█▄▀▪ ▀█▄▀▪.▀▀▀  ▀▀▀▀ ▀▀▀·▀▀▀▀  
 
-  Ξ TITLE  : Folow Like Direct Message (Followers Target)
-  Ξ NOTE   : U can add more targets, use u best ratio!
-  Ξ UPDATE : Wednesday, August 4, 2021 (GMT+8)
-           : TESTED "OK" BUG? YouTellMe!
     }`
     );
     const questions = [
